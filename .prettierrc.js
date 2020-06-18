bdbd75d3d1b82a48e9c9afc544b56a9d3d1b5652
Kwinten <kwinten.yc.li@gmail.com>
2020-06-18 19:15:54 +0800

    style: Add eslint husky and prettier to format code

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  singleQuote: true,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+};
