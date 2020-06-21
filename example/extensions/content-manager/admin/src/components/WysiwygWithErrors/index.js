babb921bf84536c9e648cb0fb105ce5791e8fb9e
Kwinten <kwinten.yc.li@gmail.com>
2020-06-21 11:43:24 +0800

    fix(core): change to use request from strapi-helper-plugin
    The request method from strapi-helper-plugin will handle all auth requested stuff and passing the jwt token when needed

--- example/extensions/content-manager/admin/src/components/WysiwygWithErrors/index.js
+++ example/extensions/content-manager/admin/src/components/WysiwygWithErrors/index.js
@@ -16,6 +16,12 @@ import Wysiwyg from '../Wysiwyg';
 import Wrapper from './Wrapper';
 import { ImageApiPanel } from 'strapi-plugin-image-api';
 
+// Use below for hotreload debugging
+
+// const {
+//   ImageApiPanel,
+// } = require("/Users/kwinten/Documents/workspace/side-project/strapi-plugin-image-api/build");
+
 // eslint-disable-next-line react/prefer-stateless-function
 class WysiwygWithErrors extends React.Component {
   render() {
